a96270cd9b70a3d4a1d42e3b7a1f53ed9de78f50
Blake Bovaird <cblakebovaird@gmail.com>
2019-07-12 14:38:43 -0400

    created for loop for the array

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,4 +1,4 @@
-// <!-- ******************** VARIABLES **************************** -->
+// Creating a variable to hold the array 
 var topics = [
     "Bear",
     "Dolphin",
@@ -11,9 +11,32 @@ var topics = [
     "Wolf",
     "Parrot"
 ];
-
+//  creating a function to re-render the HTML to display the appropriate content
 function displayAnimals(){
     var topic = $(this).attr("data-name");
     var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=rN0JfRZ29E6XebrinGCawK9xEnwbv1bx" + topic;
-    
+
+    // Creating an AJAX call for the specific animal being clicked
+    $.ajax({
+        url : queryUrl,
+        method: "GET"
+    }).then(function(response) {
+
+    });
+
+}
+
+// function to display buttons
+function displayButtons () {
+    // deletes buttons before adding new buttons so that it doesnt repeat buttons
+    $("#buttons-view").empty();
+
+    // Looping through the topics array
+    for (var i = 0; i < topics.length; i++) {
+        var a = $("<button>");
+        a.addClass("topic-btn");
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
+        $("#buttons-view").append(a);
+    }
 }
